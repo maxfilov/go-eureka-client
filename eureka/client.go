ac2c488dd915903c6e1a9502a8a08b80a7de4890
Maksim Panfilov <maksim.panfilov@diabolocom.com>
2023-05-11 15:24:22 +0200

    introduction of not found errors minor refactoring

--- eureka/client.go
+++ eureka/client.go
@@ -1,6 +1,7 @@
 package eureka
 
 import (
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
@@ -8,12 +9,14 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math/rand"
 	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"path"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -370,11 +373,11 @@ func (c *Client) RegisterInstance(appId string, instanceInfo *InstanceInfo) erro
 
 	resp, err := c.Post("apps/"+appId, body)
 	if err != nil {
-		return fmt.Errorf("cannot register application's [appId=%s] instance: %w", appId, err)
+		return fmt.Errorf("cannot register application instance: %w", err)
 	}
 
 	if resp.StatusCode != http.StatusNoContent {
-		return fmt.Errorf("cannot register application's [appId=%s] instance: unexpected code: %d", appId, resp.StatusCode)
+		return fmt.Errorf("cannot register application instance: unexpected code: %d", resp.StatusCode)
 	}
 	return nil
 }
@@ -383,17 +386,19 @@ func (c *Client) RegisterInstance(appId string, instanceInfo *InstanceInfo) erro
 func (c *Client) UnregisterInstance(appId, instanceId string) error {
 	resp, err := c.Delete("apps/" + appId + "/" + instanceId)
 	if err != nil {
-		return fmt.Errorf("cannot de-register application [appId=%s] instance [instanceId=%s]: %w", appId, instanceId, err)
+		return fmt.Errorf("cannot de-register application instance: %w", err)
 	}
-	if resp.StatusCode != http.StatusOK {
+	switch resp.StatusCode {
+	case http.StatusOK:
+		return nil
+	case http.StatusNotFound:
+		return ErrInstanceNotFound
+	default:
 		return fmt.Errorf(
-			"cannot de-register application [appId=%s] instance [instanceId=%s]: unexpected code: %d",
-			appId,
-			instanceId,
+			"cannot de-register application instance: unexpected code: %d",
 			resp.StatusCode,
 		)
 	}
-	return err
 }
 
 // SendHeartbeat Send application instance heartbeat
@@ -403,12 +408,13 @@ func (c *Client) SendHeartbeat(appId, instanceId string) error {
 		return err
 	}
 	switch resp.StatusCode {
+	case http.StatusOK:
+		return nil
 	case http.StatusNotFound:
-		return newError(
-			ErrCodeInstanceNotFound,
-			"Instance resource not found when sending heartbeat", 0)
+		return ErrInstanceNotFound
+	default:
+		return fmt.Errorf("cannot send heartbeat: unexpected code: %d", resp.StatusCode)
 	}
-	return nil
 }
 
 // GetApplications Query for all instances
@@ -418,71 +424,364 @@ func (c *Client) GetApplications() (*Applications, error) {
 		return nil, fmt.Errorf("cannot retrieve applications: %w", err)
 	}
 
-	if resp.StatusCode != http.StatusOK {
+	switch resp.StatusCode {
+	case http.StatusOK:
+		var applications Applications
+		err = xml.Unmarshal(resp.Body, &applications)
+		return &applications, err
+	default:
 		return nil, fmt.Errorf("cannot retrieve applications: unexpected code: %d", resp.StatusCode)
 	}
-	var applications Applications
-	err = xml.Unmarshal(resp.Body, &applications)
-	return &applications, err
 }
 
 // GetApplication Query for all appID instances
 func (c *Client) GetApplication(appId string) (*Application, error) {
 	resp, err := c.Get("apps/" + appId)
 	if err != nil {
-		return nil, fmt.Errorf("cannot retrieve application [appId=%s]: %w", appId, err)
+		return nil, fmt.Errorf("cannot retrieve application: %w", err)
 	}
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("cannot retrieve application [appId=%s]: unexpected code: %d", appId, resp.StatusCode)
+
+	switch resp.StatusCode {
+	case http.StatusOK:
+		var application *Application = new(Application)
+		err = xml.Unmarshal(resp.Body, application)
+		return application, err
+	case http.StatusNotFound:
+		return nil, ErrApplicationNotFound
+	default:
+		return nil, fmt.Errorf("cannot retrieve application: unexpected code: %d", resp.StatusCode)
 	}
-	var application *Application = new(Application)
-	err = xml.Unmarshal(resp.Body, application)
-	return application, err
 }
 
 // GetInstance Query for a specific appID/instanceID
 func (c *Client) GetInstance(appId, instanceId string) (*InstanceInfo, error) {
 	resp, err := c.Get("apps/" + appId + "/" + instanceId)
 	if err != nil {
-		return nil, fmt.Errorf("cannot retrieve application's [appId=%s] instance [instanceId=%s]: %w", appId, instanceId, err)
+		return nil, fmt.Errorf("cannot retrieve application instance: %w", err)
 	}
 
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf(
-			"cannot retrieve application's [appId=%s] instance [instanceId=%s]: unexpected code: %d",
-			appId,
-			instanceId,
-			resp.StatusCode)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		var instance InstanceInfo
+		err = xml.Unmarshal(resp.Body, &instance)
+		return &instance, err
+	case http.StatusNotFound:
+		return nil, ErrInstanceNotFound
+	default:
+		return nil, fmt.Errorf("cannot retrieve application instance: unexpected code: %d", resp.StatusCode)
 	}
-	var instance InstanceInfo
-	err = xml.Unmarshal(resp.Body, &instance)
-	return &instance, err
 }
 
 // GetVIP Query for all instances under a particular vip address
 func (c *Client) GetVIP(vipAddress string) (*Applications, error) {
 	resp, err := c.Get("vips/" + vipAddress)
 	if err != nil {
-		return nil, fmt.Errorf("cannot retrieve instances for vipAddress='%s': %w", vipAddress, err)
+		return nil, fmt.Errorf("cannot retrieve instances: %w", err)
 	}
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("cannot retrieve instances for vipAddress='%s': unexpected code: %d", vipAddress, resp.StatusCode)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		var applications Applications
+		err = xml.Unmarshal(resp.Body, &applications)
+		return &applications, err
+	case http.StatusNotFound:
+		return nil, ErrApplicationNotFound
+	default:
+		return nil, fmt.Errorf("cannot retrieve instances: unexpected code: %d", resp.StatusCode)
 	}
-	var applications Applications
-	err = xml.Unmarshal(resp.Body, &applications)
-	return &applications, err
 }
 
 // GetSVIP Query for all instances under a particular secure vip address
 func (c *Client) GetSVIP(svipAddress string) (*Applications, error) {
 	resp, err := c.Get("svips/" + svipAddress)
 	if err != nil {
-		return nil, fmt.Errorf("cannot retrieve instances for secure virtual IP address '%s': %w", svipAddress, err)
+		return nil, fmt.Errorf("cannot retrieve instances: %w", err)
 	}
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("cannot retrieve instances for secure virtual IP address '%s': unexpected code: %d", svipAddress, resp.StatusCode)
+		return nil, fmt.Errorf("cannot retrieve instances: unexpected code: %d", resp.StatusCode)
 	}
 	var applications *Applications = new(Applications)
 	err = xml.Unmarshal(resp.Body, applications)
 	return applications, err
+}
+
+// getCancelable issues a cancelable GET request
+func (c *Client) getCancelable(
+	endpoint string,
+	cancel <-chan bool) (*RawResponse, error) {
+	_debugf("get %s [%s]", endpoint, c.Cluster.Leader)
+	p := endpoint
+
+	req := NewRawRequest("GET", p, nil, cancel)
+	resp, err := c.SendRequest(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// get issues a GET request
+func (c *Client) Get(endpoint string) (*RawResponse, error) {
+	return c.getCancelable(endpoint, nil)
+}
+
+// put issues a PUT request
+func (c *Client) Put(endpoint string, body []byte) (*RawResponse, error) {
+
+	_debugf("put %s, %s, [%s]", endpoint, body, c.Cluster.Leader)
+	p := endpoint
+
+	req := NewRawRequest("PUT", p, body, nil)
+	resp, err := c.SendRequest(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// post issues a POST request
+func (c *Client) Post(endpoint string, body []byte) (*RawResponse, error) {
+	_debugf("post %s, %s, [%s]", endpoint, body, c.Cluster.Leader)
+	p := endpoint
+
+	req := NewRawRequest("POST", p, body, nil)
+	resp, err := c.SendRequest(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// delete issues a DELETE request
+func (c *Client) Delete(endpoint string) (*RawResponse, error) {
+	_debugf("delete %s [%s]", endpoint, c.Cluster.Leader)
+	p := endpoint
+
+	req := NewRawRequest("DELETE", p, nil, nil)
+	resp, err := c.SendRequest(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (c *Client) SendRequest(rr *RawRequest) (*RawResponse, error) {
+
+	var req *http.Request
+	var resp *http.Response
+	var httpPath string
+	var err error
+	var respBody []byte
+
+	var numReqs = 1
+
+	checkRetry := c.CheckRetry
+	if checkRetry == nil {
+		checkRetry = DefaultCheckRetry
+	}
+
+	cancelled := make(chan bool, 1)
+	reqLock := new(sync.Mutex)
+
+	if rr.cancel != nil {
+		cancelRoutine := make(chan bool)
+		defer close(cancelRoutine)
+
+		go func() {
+			select {
+			case <-rr.cancel:
+				cancelled <- true
+				_debugf("send.request is cancelled")
+			case <-cancelRoutine:
+				return
+			}
+
+			// Repeat canceling request until this thread is stopped
+			// because we have no idea about whether it succeeds.
+			for {
+				reqLock.Lock()
+				c.httpClient.Transport.(*http.Transport).CancelRequest(req)
+				reqLock.Unlock()
+
+				select {
+				case <-time.After(100 * time.Millisecond):
+				case <-cancelRoutine:
+					return
+				}
+			}
+		}()
+	}
+
+	// If we connect to a follower and consistency is required, retry until
+	// we connect to a leader
+	sleep := 25 * time.Millisecond
+	maxSleep := time.Second
+
+	for attempt := 0; ; attempt++ {
+		if attempt > 0 {
+			select {
+			case <-cancelled:
+				return nil, ErrRequestCancelled
+			case <-time.After(sleep):
+				sleep = sleep * 2
+				if sleep > maxSleep {
+					sleep = maxSleep
+				}
+			}
+		}
+
+		_debugf("Connecting to eureka: attempt %d for %s", attempt+1, rr.relativePath)
+
+		httpPath = c.getHttpPath(false, rr.relativePath)
+
+		_debugf("send.request.to %s | method %s", httpPath, rr.method)
+
+		req, err := func() (*http.Request, error) {
+			reqLock.Lock()
+			defer reqLock.Unlock()
+
+			if req, err = http.NewRequest(rr.method, httpPath, bytes.NewReader(rr.body)); err != nil {
+				return nil, err
+			}
+
+			req.Header.Set(
+				"Content-Type",
+				"application/json")
+			return req, nil
+		}()
+
+		if err != nil {
+			return nil, err
+		}
+
+		resp, err = c.httpClient.Do(req)
+		defer func() {
+			if resp != nil {
+				resp.Body.Close()
+			}
+		}()
+
+		// If the request was cancelled, return ErrRequestCancelled directly
+		select {
+		case <-cancelled:
+			return nil, ErrRequestCancelled
+		default:
+		}
+
+		numReqs++
+
+		// network error, change a machine!
+		if err != nil {
+			_errorf("network error: %v", err.Error())
+			lastResp := http.Response{}
+			if checkErr := checkRetry(c.Cluster, numReqs, lastResp, err); checkErr != nil {
+				return nil, checkErr
+			}
+
+			c.Cluster.switchLeader(attempt % len(c.Cluster.Machines))
+			continue
+		}
+
+		// if there is no error, it should receive response
+		_debugf("recv.response.from %s", httpPath)
+
+		if validHttpStatusCode[resp.StatusCode] {
+			// try to read byte code and break the loop
+			respBody, err = io.ReadAll(resp.Body)
+			if err == nil {
+				_debugf("recv.success %s", httpPath)
+				break
+			}
+			// ReadAll error may be caused due to cancel request
+			select {
+			case <-cancelled:
+				return nil, ErrRequestCancelled
+			default:
+			}
+
+			if err == io.ErrUnexpectedEOF {
+				// underlying connection was closed prematurely, probably by timeout
+				// TODO: empty body or unexpectedEOF can cause http.Transport to get hosed;
+				// this allows the client to detect that and take evasive action. Need
+				// to revisit once code.google.com/p/go/issues/detail?id=8648 gets fixed.
+				respBody = []byte{}
+				break
+			}
+		}
+
+		// if resp is TemporaryRedirect, set the new leader and retry
+		if resp.StatusCode == http.StatusTemporaryRedirect {
+			u, err := resp.Location()
+
+			if err != nil {
+				_warningf("%v", err)
+			} else {
+				// Update cluster leader based on redirect location
+				// because it should point to the leader address
+				c.Cluster.updateLeaderFromURL(u)
+				_debugf("recv.response.relocate %s", u.String())
+			}
+			resp.Body.Close()
+			continue
+		}
+
+		if checkErr := checkRetry(
+			c.Cluster, numReqs, *resp,
+			errors.New("Unexpected HTTP status code")); checkErr != nil {
+			return nil, checkErr
+		}
+		resp.Body.Close()
+	}
+
+	r := &RawResponse{
+		StatusCode: resp.StatusCode,
+		Body:       respBody,
+		Header:     resp.Header,
+	}
+
+	return r, nil
+}
+
+// DefaultCheckRetry defines the retrying behaviour for bad HTTP requests
+// If we have retried 2 * machine number, stop retrying.
+// If status code is InternalServerError, sleep for 200ms.
+func DefaultCheckRetry(
+	cluster *Cluster, numReqs int, lastResp http.Response,
+	_ error) error {
+
+	if numReqs >= 2*len(cluster.Machines) {
+		return ErrEurekaNotReachable
+	}
+
+	code := lastResp.StatusCode
+	if code == http.StatusInternalServerError {
+		time.Sleep(time.Millisecond * 200)
+
+	}
+
+	_warningf("bad response status code %d", code)
+	return nil
+}
+
+func (c *Client) getHttpPath(random bool, s ...string) string {
+	var machine string
+	if random {
+		machine = c.Cluster.Machines[rand.Intn(len(c.Cluster.Machines))]
+	} else {
+		machine = c.Cluster.Leader
+	}
+
+	fullPath := machine
+	for _, seg := range s {
+		fullPath += "/" + seg
+	}
+
+	return fullPath
 }
--- eureka/error.go
+++ eureka/error.go
@@ -1,50 +1,13 @@
 package eureka
 
 import (
-	"encoding/json"
 	"fmt"
 )
 
-const (
-	ErrCodeEurekaNotReachable = 501
-	ErrCodeInstanceNotFound   = 502
-)
-
+// Errors introduced by handling requests
 var (
-	errorMap = map[int]string{
-		ErrCodeInstanceNotFound:   "Instance resource not found",
-		ErrCodeEurekaNotReachable: "All the given peers are not reachable",
-	}
+	ErrRequestCancelled    error = fmt.Errorf("sending request is cancelled")
+	ErrInstanceNotFound    error = fmt.Errorf("instance not found")
+	ErrApplicationNotFound error = fmt.Errorf("application not found")
+	ErrEurekaNotReachable  error = fmt.Errorf("all the given peers are not reachable: tried to connect to each peer twice and failed")
 )
-
-type EurekaError struct {
-	ErrorCode int    `json:"errorCode"`
-	Message   string `json:"message"`
-	Cause     string `json:"cause,omitempty"`
-	Index     uint64 `json:"index"`
-}
-
-func (e EurekaError) Error() string {
-	return fmt.Sprintf("%v: %v (%v) [%v]", e.ErrorCode, e.Message, e.Cause, e.Index)
-}
-
-func newError(errorCode int, cause string, index uint64) *EurekaError {
-	return &EurekaError{
-		ErrorCode: errorCode,
-		Message:   errorMap[errorCode],
-		Cause:     cause,
-		Index:     index,
-	}
-}
-
-func handleError(b []byte) error {
-	eurekaErr := new(EurekaError)
-
-	err := json.Unmarshal(b, eurekaErr)
-	if err != nil {
-		_warningf("cannot unmarshal eureka error: %v", err)
-		return err
-	}
-
-	return eurekaErr
-}
